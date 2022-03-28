43badaadcdfaa7c3bb8769621f655eeb9f5079cc
Bryson <bryson.meiling@gmail.com>
2022-03-28 10:20:41 -0400

    jftdc

--- pages/_app.js
+++ pages/_app.js
@@ -2,21 +2,20 @@ import "../styles/globals.css";
 import Head from "next/head";
 import React from "react";
 import { useState } from "react";
-
-const stateContext = React.createContext({ state: {}, setState: undefined });
+import { AppWrapper } from "../components/context";
 
 function MyApp({ Component, pageProps }) {
   const [state, setState] = useState({ madeWithlove: true });
   return (
     <>
-      <stateContext.Provider value={{ state, setState }}>
-        <Head>
-          {/* adding these meta tags to remove default search bar opening on mobile, for full screen app feel. doesn't really work*/}
-          <meta name="mobile-web-app-capable" content="yes" />
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-        </Head>
+      <Head>
+        {/* adding these meta tags to remove default search bar opening on mobile, for full screen app feel. doesn't really work*/}
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+      </Head>
+      <AppWrapper>
         <Component {...pageProps} />
-      </stateContext.Provider>
+      </AppWrapper>
     </>
   );
 }
--- pages/categories.js
+++ pages/categories.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Head from "next/head";
+import Image from "next/image";
 import React from "react";
 import { useContext } from "react";
 import {
@@ -9,19 +10,35 @@ import {
   get_all_categories,
 } from "../components/database";
 import stateContext from "./_app.js";
+import { useAppContext } from "../components/context";
 export default function CategoriesPage() {
-  const context = useContext(stateContext);
-  debugger;
+  const context = useAppContext();
+  console.log(context);
   return (
     <>
       <Head>
         <title>Categories</title>
       </Head>
-      <h1>Categories</h1>
-      <Link href="/">
-        <a>Back to Home</a>
-      </Link>
-      <div>Here is the state: {context}</div>
+      <div className="min-h-max grow bg-gray-50 py-6 flex flex-col justify-center relative sm:py-12">
+        <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-center "></div>
+        <div className="relative px-6 pt-10 pb-8 bg-white shadow-xl ring-1 ring-gray-900/5 sm:max-w-lg sm:mx-auto sm:rounded-lg sm:px-10">
+          <div className="flex justify-between">
+            <h1 className="text-xl font-bold ">Categories</h1>
+            <Link href="/">
+              <a className="">
+                <Image
+                  src="/images/cottage_black_24dp.svg"
+                  alt="home"
+                  className="p-1"
+                  width={40}
+                  height={40}
+                />
+              </a>
+            </Link>
+          </div>
+          <p>Here is the state okay: {}</p>
+        </div>
+      </div>
     </>
   );
 }
